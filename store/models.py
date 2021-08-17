600fa16a47472fce3a3a1eb8fe093942d7d53c19
Billy <billywillneverreturn@gmail.com>
2021-08-17 15:56:21 +0300

    add new relationship Many-to-Many between Product and Promotion

--- store/models.py
+++ store/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 
+
+class Promotion(models.Model):
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
+
+
 class Collection(models.Model):
     title = models.CharField(max_length=255) 
+
+
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
@@ -9,6 +17,7 @@ class Product(models.Model):
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection,on_delete=models.PROTECT)
+    promotions = models.ManyToManyField(Promotion)
 
 
 class Customer(models.Model):
