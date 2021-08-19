9a8b42171fd074047ff15c34499d6c2e7aac83ae
Billy <billywillneverreturn@gmail.com>
2021-08-19 18:09:32 +0300

    refactor admin site view

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,16 @@
+from django.db.models.expressions import Col
+from django.db.models.fields import DecimalField
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+from django.core.exceptions import ObjectDoesNotExist 
+from django.contrib.contenttypes.models import ContentType
+from django.db.models import Q, F, Value, Func, ExpressionWrapper
+from django.db.models.aggregates import Count, Max, Min, Avg
+from django.db.models.functions import Concat
+from django.db import transaction, connection
+from store.models import Collection, Product, OrderItem, Order, Customer
+from tags.models import Tag, TaggedItem
 
 def say_hello(request):
-    return render(request, 'hello.html', {'name': 'Andrey'})
+    
+    return render(request, 'hello.html', {'name': 'Andrey'}) 
